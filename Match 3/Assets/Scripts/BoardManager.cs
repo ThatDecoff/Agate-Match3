2194c04b1a892d2562415e38a0d359ca4120b1e4
Adrian Kaiser <adriankaiser.rian@gmail.com>
2021-10-10 16:28:43 +0700

    added swapping function

--- Match 3/Assets/Scripts/BoardManager.cs
+++ Match 3/Assets/Scripts/BoardManager.cs
@@ -37,6 +37,16 @@ public class BoardManager : MonoBehaviour
 
     private TileController[,] tiles;
 
+    public bool IsAnimating
+    {
+        get
+        {
+            return IsSwapping;
+        }
+    }
+
+    public bool IsSwapping { get; set; }
+
     private void Start()
     {
         Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
@@ -56,7 +66,7 @@ public class BoardManager : MonoBehaviour
         {
             for (int y = 0; y < size.y; y++)
             {
-                TileController newTile = Instantiate(tilePrefab, new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * x), startPosition.y + ((tileSize.y + offsetTile.y) * y)), tilePrefab.transform.rotation, tileParent).GetComponent<TileController>();
+                TileController newTile = Instantiate(tilePrefab, new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * x), startPosition.y + ((tileSize.y + offsetTile.y) * y)), tilePrefab.transform.rotation, transform).GetComponent<TileController>();
                 tiles[x, y] = newTile;
 
                 // get no tile id
@@ -88,5 +98,84 @@ public class BoardManager : MonoBehaviour
         }
 
         return possibleId;
+    }
+
+    #region Swapping
+
+    public IEnumerator SwapTilePosition(TileController a, TileController b, System.Action onCompleted)
+    {
+        IsSwapping = true;
+
+        Vector2Int indexA = GetTileIndex(a);
+        Vector2Int indexB = GetTileIndex(b);
+
+        tiles[indexA.x, indexA.y] = b;
+        tiles[indexB.x, indexB.y] = a;
+
+        a.ChangeId(a.id, indexB.x, indexB.y);
+        b.ChangeId(b.id, indexA.x, indexA.y);
+
+        bool isRoutineACompleted = false;
+        bool isRoutineBCompleted = false;
+
+        StartCoroutine(a.MoveTilePosition(GetIndexPosition(indexB), () => { isRoutineACompleted = true; }));
+        StartCoroutine(b.MoveTilePosition(GetIndexPosition(indexA), () => { isRoutineBCompleted = true; }));
+
+        yield return new WaitUntil(() => { return isRoutineACompleted && isRoutineBCompleted; });
+
+        onCompleted?.Invoke();
+
+        IsSwapping = false;
+    }
+
+    #endregion
+
+    public Vector2Int GetTileIndex(TileController tile)
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                if (tile == tiles[x, y]) return new Vector2Int(x, y);
+            }
+        }
+
+        return new Vector2Int(-1, -1);
+    }
+
+    public Vector2 GetIndexPosition(Vector2Int index)
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        return new Vector2(startPosition.x + ((tileSize.x + offsetTile.x) * index.x), startPosition.y + ((tileSize.y + offsetTile.y) * index.y));
+    }
+
+    public List<TileController> GetAllMatches()
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                List<TileController> tileMatched = tiles[x, y].GetAllMatches();
+
+                // just go to next tile if no match
+                if (tileMatched == null || tileMatched.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (TileController item in tileMatched)
+                {
+                    // add only the one that is not added yet
+                    if (!matchingTiles.Contains(item))
+                    {
+                        matchingTiles.Add(item);
+                    }
+                }
+            }
+        }
+
+        return matchingTiles;
     }
 }
